f2fcbe37c811e4bb7c50aafe1cc7d0f53214ba67
Mariklimova <mariku22@yandex.ru>
2024-06-01 23:03:41 +0300

    added home page

--- client/src/page/Auth/Auth.jsx
+++ client/src/page/Auth/Auth.jsx
@@ -8,16 +8,17 @@ function Auth() {
             <p>Log in to access your account or create one. Verify your email for seamless access.</p>
 
             <div>
-
                 <TextField  className = {style.textField} id="outlined-basic" label="Your emali" variant="outlined" />
             </div>
             <div>
                 <TextField className = {style.textField} id="outlined-basic" label="password & Must be at least 8 characters." variant="outlined"  />
             </div>
-            <div>
-                <Button variant="contained">Continue</Button>
+            <div className = {style.button}>
+            <Button variant="contained">Continue</Button>
             </div>
+            <div className={style.link}>
             <p>Not registered yet? Sign Up</p>
+            </div>
         </div>
     </>
 }
--- client/src/page/Home/Home.jsx
+++ client/src/page/Home/Home.jsx
@@ -1,5 +1,13 @@
+import style from './home.module.css'
+
 function Home() {
-    return<>
+    return <>
+        <div className={style.wrapper}>
+            <div className={style.img}></div>
+            <h1>Woohoo!</h1>
+            <p>You have successfully logged into the system, and a token has been
+                assigned to you. To log out, please click on 'Sign Out'.</p>
+        </div>
     </>
 }
 export default Home;
